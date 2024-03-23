5a87006cd689c3895dfe15b61b0cc8e799f75f36
Alla Kostomarova <alla.kostomarova@mail.ru>
2024-03-23 16:06:45 +0200

    add Xpath Locators

--- src/test/java/Selectors.java
+++ src/test/java/Selectors.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -9,5 +11,20 @@ public class Selectors {
     public void startTrello(){
         driver = new ChromeDriver();
         driver.get("https://trello.com/");
+
+        WebElement titleLogin = driver.findElement(By.xpath("//a[@data-uuid='MJFtCCgVhXrVl7v9HA7EH_login']"));
+        System.out.println("--> " + titleLogin.getText());
+
+        WebElement btnGetTrelloForFree = driver.findElement(By.xpath("//a[text()='Get Trello for free']"));
+        System.out.println("attribute --> " + btnGetTrelloForFree.getAttribute("href"));
+
+        WebElement btnSignUpItsFree =driver.findElement(By.xpath("//form[@data-testid='ui-email-signup-form']/div/button"));
+        System.out.println("attribute --> " + btnSignUpItsFree.getAttribute("aria-label"));
+
+        WebElement btnSignUpItsFree2 =driver.findElement(By.xpath("//form[@data-testid='ui-email-signup-form']//button"));
+        System.out.println("attribute2 --> " + btnSignUpItsFree2.getAttribute("aria-label"));
+
+
+        driver.quit();
     }
 }
